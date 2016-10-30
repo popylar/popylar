1947beed4725cd09a9897f482f6b2585407bcc89
arokem <arokem@gmail.com>
2016-10-30 13:52:25 -0700

    Get rid of miniconda altogether.

--- popylar/tests/test_popylar.py
+++ popylar/tests/test_popylar.py
@@ -3,15 +3,18 @@ import popylar
 
 def test_track_event():
     """
-    Test that event tracking
+    Test that event tracking goes through
     """
-    from popylar.version import VERSION
-
-    # Test with the software version:
-    r = popylar.track_event("UA-86484662-2", 'test', 'test_track_event',
-                            software_version=VERSION)
+    r = popylar.track_event("UA-86484662-2", 'test', 'test_track_event')
     assert r.status_code == 200, "response is not OK"
 
-    # Run without the software version
-    r = popylar.track_event("UA-86484662-2", 'test', 'test_track_event')
+
+def test_track_event_with_version():
+    """
+    Test recording the software version (in this case, popylar version)
+    """
+    from popylar.version import VERSION
+    r = popylar.track_event("UA-86484662-2", 'test',
+                            'test_track_event_with_version',
+                            software_version=VERSION)
     assert r.status_code == 200, "response is not OK"
