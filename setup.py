69d51b110a77306a6b5debce7c00044a4ed51472
R. Biswas <rbiswas4@gmail.com>
2016-12-13 11:03:19 -0800

    Changed the use of configparser to make it consistent in both python 2 and 3 	modified:   popylar/popylar.py 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -14,10 +14,10 @@ popylar_path = op.join(op.expanduser('~'), '.popylar')
 # If UID file does not exist, then generate a UID and save to file.
 # We don't overwrite an existing one in order to keep UIDs constant
 # when the package is upgraded or installed in a venv.
+
 if not os.path.exists(popylar_path):
-    parser = configparser.ConfigParser()
-    parser.read_dict(dict(user=dict(uid=uuid.uuid1().hex,
-                                    track=True)))
+    default_dict = dict(user=dict(uid=uuid.uuid1().hex,  track=True))
+    parser = configparser.ConfigParser(default_dict)
     with open(popylar_path, 'w') as fhandle:
         parser.write(fhandle)
 
