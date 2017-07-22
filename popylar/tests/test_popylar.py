a3a47ed625e4f257600864a1f3eebe11d0126f0f
arokem <arokem@gmail.com>
2017-07-22 06:25:13 -0700

    Add testing of opt-in opt-out.
    Does this have any un-intended side effects?

--- popylar/tests/test_popylar.py
+++ popylar/tests/test_popylar.py
@@ -18,3 +18,12 @@ def test_track_event_with_version():
                             'test_track_event_with_version',
                             software_version=VERSION)
     assert r.status_code == 200, "response is not OK"
+
+
+def test_opt():
+    popylar.opt_out()
+    parser = popylar.get_or_create_config()
+    assert parser['user']['track'] == "False"
+    popylar.opt_in()
+    parser = popylar.get_or_create_config()
+    assert parser['user']['track'] == "True"
